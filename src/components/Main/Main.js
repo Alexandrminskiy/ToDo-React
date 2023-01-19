dafdcb93cd2c5729c2c8c308b2a75901a342bdd5
Alexandrminskiy <alexandrminskiy@gmail.com>
2023-01-19 11:01:19 +0300

    123

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,7 @@
 export function Main({ list }) {
   return (
     <main className="py-5">
+      123
       <ul class="list-group">
         {list.map((eachTodo, index) =>
           <li key={index + eachTodo.id} class="list-group-item">{eachTodo.title}</li>
